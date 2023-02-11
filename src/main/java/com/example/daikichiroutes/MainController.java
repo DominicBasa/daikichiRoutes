30e16d5d21cb036780d2f75009776702e801eca4
DominicBasa <steelersfan2990@gmail.com>
2023-02-10 22:38:37 -0600

    Updated

---
+++ src/main/java/com/example/daikichiroutes/MainController.java
@@ -0,0 +1,26 @@
+package com.example.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+  
+  @RequestMapping("/daikichi")
+  public String index() {
+    return "Welcome";
+  }
+  
+  @RequestMapping("/daikichi/today")
+  public String today() {
+    return "Today you will find luck in all your endeavors!";
+  }
+  
+  @RequestMapping("/daikichi/tomorrow")
+  public String tomorrow() {
+    return "Tomorrow, an oppurtunity will arise, so be sure to be open to new ideas!";
+  
+  }
+
+
+}
